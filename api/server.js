38021db531fa0e20c00f9d40b4dadd8e9ce6ec70
rebecalazarini <rebecalazarini2007@gmail.com>
2025-09-23 16:27:58 -0300

    srver.js limpo

--- api/server.js
+++ api/server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const { PrismaClient } = require("@prisma/client");
 require("dotenv").config();
 
 const routes = require('../src/routes');
 
-const prisma = new PrismaClient();
 const app = express();
 
 // Middleware
@@ -16,5 +14,3 @@ app.use('/', routes);
 app.listen(3000, () => {
     console.log(`Servidor rodando em http://localhost:3000`);
 });
-
-
