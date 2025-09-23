5ebff1675dba4c9e8e9e905840006e6213ac8da5
rebecalazarini <rebecalazarini2007@gmail.com>
2025-09-23 15:49:06 -0300

    modificações

--- api/server.js
+++ api/server.js
@@ -14,8 +14,6 @@ app.use(cors());
 app.use(express.json()); 
 app.use('/', routes);
 
-
-   // Servidor rodando
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
